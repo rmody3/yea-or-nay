b0b6af82c84be0ddc248d319f8ff6e0b203614fc
rmody3 <rmody3@gmail.com>
2017-03-28 20:32:36 -0400

    added votes by rep

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const baseurl = "https://api.propublica.org/congress/v1/"
 
+
 const google = `https://www.googleapis.com/civicinfo/v2/representatives?roles=legislatorUpperBody&roles=legislatorLowerBody&levels=country&key=${googleKey}&address=`
 var allReps = {}
-
+var yourReps = []
 // function convertAddress(address){
 //   return address.replace(" ", "%20")
 // }
@@ -11,14 +12,20 @@ $(function () {
   getRepList("house")
   getRepList("senate")
   $("#address-search").on("submit", function(event) {
+    yourReps = []
     event.preventDefault()
     var address = $('#address').val()
     $.get(google + address, (result)=> {
       var reps = result["officials"]
+      reps.forEach((rep)=>{
+        var aRep = new Representative(rep.name)
+        aRep.findMemberId()
+        yourReps.push(aRep)
+      })
       var repTable = "<tr><th>Your Representatives</th></tr>"
-      repTable += `<tr><td><a href="#" data-id= "rep-link"> Senate: ${reps[0].name} </a></td></tr>`
-      repTable += `<tr><td><a href="#" class= "rep-link"> Senate: ${reps[1].name}</a></td></tr>`
-      repTable += `<tr><td><a href="#" class= "rep-link"> Congress: ${reps[2].name}</a></td></tr>`
+      repTable += `<tr><td><a href="#" class="rep-link" data-id= "${yourReps[0].memberId}"> Senate: ${reps[0].name} </a></td></tr>`
+      repTable += `<tr><td><a href="#" class="rep-link" data-id= "${yourReps[1].memberId}"> Senate: ${reps[1].name}</a></td></tr>`
+      repTable += `<tr><td><a href="#" class="rep-link" data-id= "${yourReps[2].memberId}"> Congress: ${reps[2].name}</a></td></tr>`
       $("#reps").html(repTable)
       addRepLinks()
     })
@@ -35,39 +42,36 @@ function getRepList(chamber) {
         let name = member.first_name + " " + member.last_name
         allReps[name] = member.id
       })
-      console.log(allReps)
     })
 }
 
 function addRepLinks(){
   $(".rep-link").on('click', function () {
-    this.dat
-  })
+    var memberId = $(this).data("id")
+    $.ajax({
+      url: baseurl + "members/"+memberId+"/votes.json",
+      headers: {"X-API-Key": propublicaKey}})
+      .done(response => {
+        var votes = response["results"][0]["votes"]
+        var voteList = ""
+        votes.forEach((vote)=>{
+          voteList += `<li>${vote.position}-${vote.description}</li>`
+        })
+        console.log(voteList);
+        $("#votes").html(voteList)
+      })
+    })
 }
 
+class Representative {
+  constructor(name, chamber) {
+    this.name = name
+    this.chamber = chamber
+  }
 
-// class Representative {
-//   constructor(chamber, firstName, lastName) {
-//     this.chamber = chamber
-//     this.firstName = firstName
-//     this.lastName = lastName
-//   }
-//
-//   function findMemberID() {
-//     $.get()
-//     return
-//   }
-// }
-
-
-//
-// var address = $('#address').val()
-// // console.log(address)
-// //
-// $.ajax({
-//   url: baseurl + "members/K000388/votes.json",
-//   headers: {"X-API-Key": ""}})
-  // .done(response =>
-  //   {$('body').text(response["results"][0]["votes"][0].position)
-  //   console.log(response["results"][0]["votes"][0])
-  //   })
+  findMemberId() {
+    this.memberId = allReps[this.name]
+    console.log(this.name, this.memberId)
+    return this.memberId
+  }
+}
